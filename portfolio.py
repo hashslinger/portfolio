81f36879902983eb6372fed722476be30e567656
hashslinger <gregoryguterman@gmail.com>
2015-10-03 11:19:35 -0700

    Worked on Contact Form

--- portfolio.py
+++ portfolio.py
@@ -3,12 +3,31 @@
     Gregory Guterman Portfolio
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+from wtforms import Form, TextField, TextAreaField, SubmitField, validators
+
+app.secret_key = '3ruqh4r0r8j230rj0w9f0kq90i23092'
+
+class ContactForm(Form):
+    name = TextField('Name', [validators.Length(min=1, max=35)])
+    email = TextField('Email', [validators.Length(min=6, max=35)])
+    message = TextAreaField('Message', [validators.DataRequired('Please enter a message')])
+    submit = SubmitField("Submit")
+
+@app.route('/email', methods=['GET', 'POST'])
+def email():
+    form = ContactForm(request.form)
+    if request.method == 'POST' and form.validate():
+        flash('Thanks for your email')
+    form = ContactForm()
+    return render_template('home.html', form=form)
+
+@app.route('/', methods=['GET'])
 def home():
-    return render_template('home.html')
+    form = ContactForm()
+    return render_template('home.html', form=form)
 
 @app.route('/games')
 def games():
